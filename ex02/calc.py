e9003d7a06897d106c923fb761e1a37472cb0097
yoshida <c0a2113935@edu.teu.ac.jp>
2022-11-29 14:46:26 +0900

    コメント

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,16 +1,46 @@
 import tkinter as tk
+import tkinter.messagebox as tkm
 
-# 練習１
+
+def button_click(event):
+    btn = event.widget
+    num = btn["text"]
+    if num == "=":
+        pass
+    else:
+
+        entry.insert(tk.END,num)
+    #tkm.showinfo("", f"{num}ボタンがクリックされました")
+
+
 root = tk.Tk()
 root.geometry("300x500")
 
-# 練習２
-r, c = 0, 0
+entry=tk.Entry(root,justify="right",width=10,font=("",40))
+entry.grid(row=0,column=0,columnspan=3)
+
+
+r, c = 1, 0
 for num in range(9, -1, -1):
     button = tk.Button(root, text=f"{num}", width=4, height=2, font=("", 30))
     button.grid(row=r, column=c)
+    button.bind("<1>", button_click)
     c += 1
     if c%3 == 0:
         r += 1
         c = 0
-root.mainloop
+
+operators =["+","="]
+for ope in operators:
+    button = tk.Button(root, text=f"{ope}", width=4, height=2, font=("", 30))
+    button.grid(row=r, column=c)
+    button.bind("<1>", button_click)
+    c += 1
+    if c%3 == 0:
+        r += 1
+        c = 0
+
+
+
+
+root.mainloop()
