a44570dd7ff22c310a1a42b5e1caf9a4062ad977
yoshida <c0a2113935@edu.teu.ac.jp>
2022-11-29 15:01:48 +0900

    コメント

--- ex02/calc.py
+++ ex02/calc.py
@@ -6,7 +6,11 @@ def button_click(event):
     btn = event.widget
     num = btn["text"]
     if num == "=":
-        pass
+        siki= entry.get()
+        res = eval(siki)
+        entry.delete(0,tk.END)
+        entry.insert(tk.END,res)
+
     else:
 
         entry.insert(tk.END,num)
